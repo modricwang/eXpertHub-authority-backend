82ca7c3c342de538de315526f61548bb32f5d3b6
Modric Wang <modricwang@126.com>
2018-06-07 00:15:41 +0800

    `access` complete

--- authority_backend/access.py
+++ authority_backend/access.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from authority_backend import models
 import json
-
+from definitions import *
 from django.http.request import QueryDict
 
 
@@ -19,20 +19,21 @@ def request_body_serialze(request):
 def handle(request):
     if request.method == 'POST':
         if 'uid' in request.POST and 'rid' in request.POST:
-            uid = int(request.POST['uid'])
-            rid = int(request.POST['rid'])
+            uid = str(request.POST['uid'])
+            rid = str(request.POST['rid'])
             models.access.objects.create(uid=uid, rid=rid)
-            s = 'ok'
+            errcode = RESPONSE_OK
         else:
-            s = 'mmp'
+            errcode = RESPONSE_ERR
+        s = {"errcode": errcode}
     elif request.method == 'GET':
         errcode = 0
         l = None
         if 'uid' in request.GET:
-            uid = int(request.GET['uid'])
+            uid = str(request.GET['uid'])
             l = models.access.objects.filter(uid=uid)
         elif 'rid' in request.GET:
-            rid = int(request.GET['rid'])
+            rid = str(request.GET['rid'])
             l = models.access.objects.filter(rid=rid)
         else:
             l = models.access.objects.all()
@@ -43,12 +44,12 @@ def handle(request):
         s = json.dumps(s)
     elif request.method == 'DELETE':
         body_dict = request_body_serialze(request)
-        # print(request.body)
-        # print(body_dict)
-        uid = int(body_dict['uid'])
-        rid = int(body_dict['rid'])
+        uid = str(body_dict['uid'])
+        rid = str(body_dict['rid'])
         models.access.objects.filter(uid=uid, rid=rid).delete()
-        s = 'ok'
+        errcode = RESPONSE_OK
+        s = {"errcode": errcode}
     else:
-        s = 'unknown operation'
+        errcode = RESPONSE_ERR
+        s = {"errcode": errcode}
     return HttpResponse(s)
